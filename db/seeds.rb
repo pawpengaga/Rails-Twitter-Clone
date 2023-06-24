2d25142c574a7d97143af66e19391340bae2bbb1
Erick Rivera <yomellamoerick.771@gmail.com>
2023-06-24 06:51:52 -0400

    important changes in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-250.times do |r|
-    Tweet.create(username: Faker::App.name, description: Faker::Lorem.paragraph_by_chars(number: 280, supplemental: false))
+650.times do |u|
+
+    dabMeUp =[
+    Faker::GreekPhilosophers.quote,
+    Faker::Quote.famous_last_words,
+    Faker::Lorem.paragraph_by_chars(number: 140, supplemental: false),
+    Faker::Marketing.buzzwords,
+    Faker::Company.catch_phrase,
+    Faker::Quote.most_interesting_man_in_the_world,
+    Faker::Quote.singular_siegler,
+    Faker::Coffee.notes
+    ]
+    
+    Tweet.create(username: Faker::Name.first_name, description: dabMeUp[rand(0..7)])
 end
