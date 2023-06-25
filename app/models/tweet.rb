a608dc417b232c2f8cf23b872684b6cf26ab0fe1
Erick Rivera <yomellamoerick.771@gmail.com>
2023-06-24 22:37:24 -0400

    dictionary changed

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -14,17 +14,6 @@ class Tweet < ApplicationRecord
         }
     }
 
-    pg_search_scope :search_full_text, against:{
-        username: 'A',
-        description: 'B'
-    },
-    using: {
-        tsearch: {
-            prefix: true,
-            any_word: true,
-            dictionary: 'spanish'
-        }
-    }
     #Revisar y preguntar luego:
     #https://www.rubydoc.info/gems/pg_search/2.0.1#dictionary
 end
